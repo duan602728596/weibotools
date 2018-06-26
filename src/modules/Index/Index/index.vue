f6c9c6bf66fbe37d3dc8d0abb4faa46f9edb674b
段昊辰 <duanhaochen@126.com>
2018-06-26 23:59:40 +0800

    添加了广告

--- src/modules/Index/Index/index.vue
+++ src/modules/Index/Index/index.vue
@@ -20,8 +20,50 @@
         <router-link to="/Checkin">一键点赞</router-link>
       </el-col>
     </el-row>
+    <!-- 广告 -->
+    <div class="guanggao" v-if="isGuanggao">
+      <div class="guanggao-box">
+        <p class="text">扫一扫，打赏一下可怜的作者。嘤嘤嘤...</p>
+        <el-row>
+          <el-col :span="12">
+            <img class="shoukuan" src="./wx.jpg">
+          </el-col>
+          <el-col :span="12">
+            <img class="shoukuan" src="./zfb.jpg">
+          </el-col>
+        </el-row>
+      </div>
+      <el-button class="close" type="danger" icon="el-icon-circle-close" @click="onCloseGuanggao()">关闭</el-button>
+    </div>
   </div>
 </template>
+
+<script type="text/javascript">
+  const process: Object = global.require('process');
+
+  let isGuanggao: boolean = do{
+    if(process.env.NODE_ENV === 'development'){
+      false;
+    }else{
+      const r: number = Math.random();
+      (r > 0.1 && r < 0.4) || (r > 0.6 && r < 0.8);
+    }
+  };
+
+  export default {
+    data(): Object{
+      return {
+        isGuanggao // 是否显示广告
+      };
+    },
+    methods: {
+      // 关闭广告
+      onCloseGuanggao(): void{
+        this.isGuanggao = false;
+      }
+    }
+  };
+</script>
 
 <style lang="scss" scoped>
   .box {
@@ -41,5 +83,36 @@
       width: 100%;
       height: 100%;
     }
+  }
+  .guanggao {
+    position: fixed;
+    z-index: 99;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, .8);
+  }
+  .guanggao-box {
+    margin: 0 auto;
+    width: 500px;
+    padding: 120px 0;
+  }
+  .text {
+    margin-bottom: 20px;
+    font-size: 24px;
+    color: #fff;
+    text-align: center;
+  }
+  .shoukuan {
+    display: block;
+    margin: 0 auto;
+    width: 230px;
+  }
+  .close {
+    position: absolute;
+    z-index: 100;
+    right: 50px;
+    top: 30px;
   }
 </style>
