354ffdf1ab3b54f606fce72680454932f404a795
duan602728596 <duanhaochen@126.com>
2018-07-26 15:44:20 +0800

    脚本添加file文件的移动

--- bin/clean.js
+++ bin/clean.js
@@ -49,6 +49,36 @@ function readdir(filePath){
   });
 }
 
+/* 移动文件 */
+function rename(oldFilePath, newFilePath){
+  return new Promise((resolve, reject)=>{
+    fs.rename(oldFilePath, newFilePath, (err)=>{
+      if(err){
+        reject(err);
+      }else{
+        resolve();
+      }
+    }).catch((err)=>{
+      console.error(err);
+    });
+  });
+}
+
+/* 创建文件夹 */
+function mkdir(filePath){
+  return new Promise((resolve, reject)=>{
+    fs.mkdir(filePath, (err)=>{
+      if(err){
+        reject(err);
+      }else{
+        resolve();
+      }
+    }).catch((err)=>{
+      console.error(err);
+    });
+  });
+}
+
 (async function(){
   try{
     // 删除dll文件
@@ -61,6 +91,19 @@ function readdir(filePath){
         break;
       }
     }
+    // 移动file文件
+    const build = path.resolve(__dirname, '../build');
+    const filePath = path.resolve(__dirname, '../file');
+    const files = await readdir(filePath);
+    const buildFile = path.resolve(build, 'file');
+    await mkdir(buildFile);
+    for(let i = 0, j = files.length; i < j; i++){
+      const item = files[i];
+      const old = path.resolve(filePath, item);
+      const new1 = path.resolve(buildFile, item);  
+      await rename(old, new1);
+    }
+    await rmdir(filePath);
   }catch(err){
     console.error(err);
   }
