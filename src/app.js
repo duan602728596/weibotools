4f9bae4cc93d2d2c8022a47d3fa8c4e5baaad60f
duan602728596 <duanhaochen@126.com>
2018-09-19 16:37:38 +0800

    调整代码

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Vue from 'vue/dist/vue';
+import classNames from 'classnames';
 import App from './AppModule.vue';
 import { storeFactory } from './store/store';
-import classNames from 'classnames';
 import routers from './router/routers';
 import './common.scss';
 import './iview';
